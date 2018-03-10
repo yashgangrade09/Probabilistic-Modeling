13adbe63a58f3f803152025e8d4f3e6e3a1fb1d4
Yash <yashgangrade09@gmail.com>
2018-03-10 14:07:15 -0700

    Assignment 2 - Base Code completed, now working on the Report

--- Assignment 2/Bayesnet.r
+++ Assignment 2/Bayesnet.r
@@ -4,21 +4,30 @@ file = read.csv("RiskFactors.csv", header = TRUE);
 #defining in order of the definitions
 variables = c("income", "exercise", "smoke", "bmi", "bp", "cholesterol", "angina", "stroke", "attack", "diabetes");
 
-#numbering
-#-------------------------------------------------------
-#1: income
-#2: exercise
-#3: smoke
-#4: bmi
-#5: bp
-#6: cholesterol
-#7: angina
-#8: stroke
-#9: attack
-#10:diabetes
-#-------------------------------------------------------
+# Indexing used to populate different probability tables. Kind of like a map from integer to string. 
+# 
+#1 -> income
+#2 -> exercise
+#3 -> smoke
+#4 -> bmi
+#5 -> bp
+#6 -> cholesterol
+#7 -> angina
+#8 -> stroke
+#9 -> attack
+#10 ->diabetes
+#
 
-#using level order traversal to populate individual conditional probability tables
+# Using level order traversal to populate individual conditional probability tables.
+#
+# Naming Convention:
+# Example: Variables are A, B, C.
+# 1) A = createCPTfromData(...) -> creates the probability distribution of A from the given csv file
+# 2) A_B = createCPTfromData(...) -> creates the probability distribution of A|B from the given csv file
+# 3) A_B_C = createCPTfromData(...) -> creates the probability distribution of A|B,C from the given csv file
+# And as follows
+# 
+
 income = createCPTfromData(x = file, varnames = c("income"));
 smoke_income = createCPTfromData(x = file[, c(3,1)], varnames = c("smoke", "income"));
 bmi_income_excercise = createCPTfromData(x = file[, c(4,1,2)], varnames = c("bmi", "income", "exercise"));
@@ -34,11 +43,3 @@ angina_bmi_bp_cholesterol = createCPTfromData(x = file[, c(7,4,5,6)], varnames =
 bayesnet = list("1" = income, "2" = smoke_income, "3" = bmi_income_excercise, "4" = exercise_income, "5" = bp_income_exercise_smoke, 
                 "6" = cholesterol_income_exercise_smoke, "7" = diabetes_bmi, "8" = stroke_bmi_bp_cholesterol, "9" = attack_bmi_bp_cholesterol, 
                 "10" = angina_bmi_bp_cholesterol);
-
-l = 0;
-for(i in 1:10){
-  l = l + nrow(bayesnet[[i]]);
-}
-#(bayesnet[[2]])
-#((nrow(bayesnet[[2]])))
-(l)
